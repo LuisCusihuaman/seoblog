98ea2453a88fe6e2671e8fd5680413cbcda73e7f
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-27 02:40:24 -0300

    chore: configured connection with db

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-//app
+// app
 const app = express();
+
+// db
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB connected"));
 
 //middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-//cors
+// cors
 if (process.env.NODE_ENV === "development") {
 	app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
