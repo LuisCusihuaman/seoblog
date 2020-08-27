d4cd999b9dae0957394637feda8ac5df4f18d003
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-27 11:55:14 -0300

    feat: signin users

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const shortId = require("shortid");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+
 exports.signup = (req, res) => {
 	User.findOne({ email: req.body.email }).exec((err, user) => {
 		if (user) {
@@ -18,5 +21,29 @@ exports.signup = (req, res) => {
 			// res.json({ user: success });
 			res.json({ message: "Signup success! Please signin." });
 		});
+	});
+};
+exports.signin = (req, res) => {
+	const { email, password } = req.body;
+	// check if user exists
+	User.findOne({ email }).exec((err, user) => {
+		if (err || !user) {
+			return res.status(400).json({
+				error: "User with that email does not exist. Please signup",
+			});
+		}
+		// authenticate
+		if (!user.authenticate(password)) {
+			return res
+				.status(400)
+				.json({ error: "Email and password do not match" });
+		}
+		// generate a jsonwebtoken and send this to client
+		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "1d",
+		});
+		res.cookie("token", token, { expiresIn: "1d" });
+		const { _id, username, name, email, role } = user;
+		return res.json({ token, user: { _id, username, name, email, role } });
 	});
 };
