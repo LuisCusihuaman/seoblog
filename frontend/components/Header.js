aba13b7dd9dc8997459d9346b66cb65bc394cede
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-27 15:24:37 -0300

    chore(frontend): added env variables

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -7,12 +7,9 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   NavbarText,
 } from 'reactstrap';
+import { APP_NAME } from '../config';
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,23 +17,21 @@ const Header = (props) => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">{APP_NAME}</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>Simple Text</NavbarText>
+      </Collapse>
+    </Navbar>
   );
 };
 
---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'SEOBLOG',
+    API_DEVElOPMENT: 'http://localhost:8000/api',
+    PRODUCTION: false,
+  },
+};
