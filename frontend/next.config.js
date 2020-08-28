3d2b85f4c7b80a3dae2897ee5732b3b4ffd98d59
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-28 08:56:24 -0300

    fix: env frontend variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicRuntimeConfig: {
     APP_NAME: 'SEOBLOG',
-    API_DEVElOPMENT: 'http://localhost:8000/api',
+    API_DEVELOPMENT: 'http://localhost:8000/api',
     PRODUCTION: false,
   },
 };
