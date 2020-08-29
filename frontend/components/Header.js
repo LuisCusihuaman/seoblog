d3133b6262237b8492ac153d97fd533b0eb03d59
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-29 12:26:01 -0300

    fix(Header): did not expect server HTML to contain

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Router from 'next/router';
 import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
 import { APP_NAME } from '../config';
 import Link from 'next/link';
 import { isAuth, signout } from '../actions/auth';
 
-const Header = (props) => {
+const Header = () => {
+  //This problem happens because the server loads the component and returns the html but the client changes it and then they don't match
+  // It’s because React expects the one rendered on the server should be the same with one rendered on client during hydration. It’s okay to use window type-checking before doing DOM access/operations but make sure it won’t affect rendering difference between client and server.
+  const [isBrowser, setIsBrowser] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    process.browser && setIsBrowser(true);
+  }, []);
 
   return (
     <Navbar color="light" light expand="md">
@@ -18,7 +24,7 @@ const Header = (props) => {
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
-          {!isAuth() && (
+          {isBrowser && !isAuth() && (
             <>
               <NavItem>
                 <Link href="/signin">
@@ -32,7 +38,7 @@ const Header = (props) => {
               </NavItem>
             </>
           )}
-          {isAuth() && (
+          {isBrowser && isAuth() && (
             <NavItem>
               <NavLink
                 style={{ cursor: 'pointer' }}
