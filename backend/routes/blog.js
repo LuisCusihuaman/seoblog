f97fcdfd45fb7c1fa81a28d8e97ecb6b193af8b9
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-30 18:06:58 -0300

    feat: created blog with photo,categories and tags

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { time } = require('../controllers/blog');
 const router = express.Router();
+const { requireSignin, adminMiddleware } = require('../controllers/auth');
+const { create } = require('../controllers/blog');
 
-router.get('/', time);
+router.post('/blog', requireSignin, adminMiddleware, create);
 
 module.exports = router;
