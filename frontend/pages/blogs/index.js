76ebb75281587b61a1871603075f0e47cec32ea0
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-09-01 02:12:24 -0300

    feat: ssr blogs page with getInitialProps

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,6 +1,45 @@
 import Layout from '../../components/Layout';
+import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
+import Link from 'next/link';
+import moment from 'moment';
+import renderHTML from 'react-render-html';
 
-export default function index() {
+export default function Blogs({ blogs, categories, tags, size }) {
+  const showAllBlogs = () => {
+    return blogs.map((blog, i) => (
+      <article key={i}>
+        <div className="lead pb-4">
+          <header>
+            <Link href={`/blogs/${blog.slug}`}>
+              <a>
+                <h2 className="display-4 pt-3 pb-3 font-weight-blog">{blog.title}</h2>
+              </a>
+            </Link>
+          </header>
+          <section>
+            <p className="mark ml-1 pt-2 pb-3">
+              Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
+            </p>
+          </section>
+          <section>
+            <p>blog categories and tags</p>
+            <div className="row">
+              <div className="col-md-4">image</div>
+              <div className="col-md-8">
+                <section>
+                  <div className="pb-3">{renderHTML(blog.excerpt)}</div>
+                  <Link href={`/blogs/${blog.slug}`}>
+                    <a className="btn btn-primary pt-2">Read more</a>
+                  </Link>
+                </section>
+              </div>
+            </div>
+          </section>
+        </div>
+        <hr />
+      </article>
+    ));
+  };
   return (
     <Layout>
       <main>
@@ -18,10 +57,25 @@ export default function index() {
         </div>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-12">show all blogs</div>
+            <div className="col-md-12">{showAllBlogs()}</div>
           </div>
         </div>
       </main>
     </Layout>
   );
 }
+
+Blogs.getInitialProps = () => {
+  return listBlogsWithCategoriesAndTags().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      return {
+        blogs: data.blogs,
+        categories: data.categories,
+        tags: data.tags,
+        size: data.size,
+      };
+    }
+  });
+};
