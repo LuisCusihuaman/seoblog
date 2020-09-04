ea8d102e204b8beb548cd49539162b0b47416021
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-09-04 11:33:51 -0300

    feat: find related blogs by tag slug

--- backend/controllers/tag.js
+++ backend/controllers/tag.js
@@ -1,4 +1,5 @@
 const Tag = require('../models/tag');
+const Blog = require('../models/blog');
 const slugify = require('slugify');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const { response, request } = require('express');
@@ -24,14 +25,20 @@ exports.list = (req, res = response) => {
   });
 };
 
-exports.read = (req = request, res = response) => {
+exports.read = async (req = request, res = response) => {
   const slug = req.params.slug.toLowerCase();
-  Tag.findOne({ slug }).exec((error, tag) => {
-    if (error) {
-      return res.status(400).json({ error: errorHandler(error) });
-    }
-    res.json(tag);
-  });
+  try {
+    const findTag = await Tag.findOne({ slug });
+    const blogs = await Blog.find({ tags: findTag })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name ')
+      .select('_id title slug excerpt categories postedBy tags createdAt updatedAt');
+    console.log({ findTag, blogs });
+    return res.json({ tag: findTag, blogs });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
 exports.remove = (req = request, res = response) => {
