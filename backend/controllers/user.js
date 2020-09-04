f0af1d85070dea49ef76ee4b0ede035271c7af7a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-09-04 17:56:17 -0300

    chore(user-route): public profile

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,31 @@
 const User = require('../models/user');
+const Blog = require('../models/blog');
 
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
   return res.json(req.profile);
+};
+exports.publicProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    user.photo = undefined;
+    const blogs = await Blog.find({ postedBy: user._id })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name ')
+      .limit(10)
+      .select('_id title slug excerpt categories tags postedBy createdAt updatedAt');
+    return res.json({ user, blogs });
+  } catch (error) {
+    const { message } = error;
+    const status = error.statusCode || 500;
+    return res.status(status).json({ error: message });
+  }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { requireSignin, authMiddleware } = require('../controllers/auth');
-const { read } = require('../controllers/user');
+const { read, publicProfile } = require('../controllers/user');
 const router = express.Router();
 
 router.get('/profile', requireSignin, authMiddleware, read);
-
+router.get('/user/:username', publicProfile);
 module.exports = router;
