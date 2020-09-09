18d1b98e78edb720b28b8b52d6b667bd94b77fd9
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-09-09 08:39:27 -0300

    perf: deleted duplicate code

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -250,29 +250,3 @@ exports.listByUser = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
-
-exports.publicProfile = async (req, res) => {
-  const { username } = req.params;
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      error = new Error('User not found');
-      error.statusCode = 404;
-      throw error;
-    }
-    user.photo = undefined;
-    user.hashed_password = undefined;
-    const blogs = await Blog.find({ postedBy: user._id })
-      .populate('categories', '_id name slug')
-      .populate('tags', '_id name slug')
-      .populate('postedBy', '_id name ')
-      .limit(10)
-      .select('_id title slug excerpt categories tags postedBy createdAt updatedAt');
-    return res.json({ user, blogs });
-  } catch (error) {
-    const { message } = error;
-    const status = error.statusCode || 500;
-    return res.status(status).json({ error: message });
-  }
-};
