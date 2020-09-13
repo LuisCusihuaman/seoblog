b84d9ba9a65a9db15a5db34a49335fed255bec2d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-09-13 19:01:52 -0300

    fix(config): env uses next.config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,13 +1,16 @@
-export const isProduction = process.env.PRODUCTION.toLowerCase() === 'true';
-export const API = process.env.PRODUCTION
-  ? process.env.API_PRODUCTION
-  : process.env.API_DEVELOPMENT;
-export const APP_NAME = process.env.APP_NAME;
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
-export const DOMAIN = process.env.PRODUCTION
-  ? process.env.DOMAIN_PRODUCTION
-  : process.env.DOMAIN_DEVELOPMENT;
+export const isProduction = publicRuntimeConfig.PRODUCTION;
+export const API = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.API_PRODUCTION
+  : publicRuntimeConfig.API_DEVELOPMENT;
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-export const FB_APP_ID = process.env.FB_APP_ID;
-export const DISQUS_SHORTNAME = process.env.DISQUS_SHORTNAME;
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.DOMAIN_PRODUCTION
+  : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
+export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID;
